37485721f3272629bbcf2842a39a80974c851d68
swrcc <swrcc@albany.edu>
2024-09-05 14:37:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pyscript import display
 n_angles = 36
 n_radii = 8
 min_radius = 0.25
-radii = np.linspace(min_radius, 0.95, n_radii)
+radii = np.linspace(min_radius, 0.5, n_radii)
 
 angles = np.linspace(0, 2 * np.pi, n_angles, endpoint=False)
 angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
